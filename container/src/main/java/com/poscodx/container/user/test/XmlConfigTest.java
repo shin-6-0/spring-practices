0fff194a44e534b08a028f1dd782d6d6c942bb1a
shin-6-0 <syk6715@naver.com>
2023-10-04 10:52:45 +0900

    container #2

---
+++ container/src/main/java/com/poscodx/container/user/test/XmlConfigTest.java
@@ -0,0 +1,78 @@
+package com.poscodx.container.user.test;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
+import com.poscodx.container.user.User;
+
+public class XmlConfigTest {
+
+	public static void main(String[] args) {
+		// XML Auto Configuration(Annotation Scanning)
+		//testApplicationContext01();	
+
+		// XML Auto Configuration(Annotation Scanning)
+		testApplicationContext02();			
+		
+		// XML Auto Configuration(Annotation Scanning)
+		// testBeanFactory01();
+		
+		// XML Bean Configuration(Explicit Scanning)
+		//testBeanFactory02();		
+		
+	}
+
+	private static void testApplicationContext01() {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("com/poscodx/container/user/applicationContext01.xml");
+		
+		User user = null;
+		//Type으로 빈 가져오기
+		user = ac.getBean(User.class);
+		System.out.println(user.getName());
+		
+		//id로 빈 가져오기
+		//Annotation Scan(Auto Configuration)에서는 Bean id 가 자동으로 부여된다. 
+		user = (User) ac.getBean("user");
+		System.out.println(user.getName());
+	}
+
+	private static void testApplicationContext02() {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("com/poscodx/container/user/applicationContext02.xml");
+		
+		User user = null;
+		
+		//Type으로 빈 가져오기
+		user = ac.getBean(User.class);
+		System.out.println(user.getName());
+		
+		//id로 빈 가져오기
+		//Annotation Scan(Auto Configuration)에서는 Bean id 가 자동으로 부여되지만,
+		//Bean Configuration 에서는 가져오지 않고 오류가 발생한다 -> No bean named 'user' available
+		//bean id = "user"라고 정해주어야함
+		user = (User) ac.getBean("user");
+		System.out.println(user.getName());
+		
+		//name으로 가져오기
+		user = (User) ac.getBean("usr");
+		System.out.println(user.getName());
+		
+		//type으로 가져오기
+		user = (User) ac.getBean(User.class);
+		System.out.println(user.getName());	
+	}	
+
+	private static void testBeanFactory01() {
+		BeanFactory bf = new XmlBeanFactory(new ClassPathResource("com/poscodx/container/user/applicationContext01.xml"));
+		User user = bf.getBean(User.class);
+		System.out.println(user.getName());
+	}
+
+	private static void testBeanFactory02() {
+		BeanFactory bf = new XmlBeanFactory(new ClassPathResource("com/poscodx/container/user/applicationContext02.xml"));
+		User user = bf.getBean(User.class);
+		System.out.println(user.getName());
+	}
+}
